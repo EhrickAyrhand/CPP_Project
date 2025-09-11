9ee7e70df35c927cf6c7a48463f97bc762c3558e
EhrickAyrhand <lordayrhand@gmail.com>
2025-09-11 00:16:58 -0300

    Feat: Learning about ifStatements switches and logicalOperatorsChallanges made temperatureConversion and Calculator project

---
+++ ternary.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    /* ternary operator ?: = replacement to an if/else statement
+       condition ? expression 1 : expression 2*/
+
+    int grade = 55;
+
+    grade >= 60 ? cout << "You pass!": cout << "You fail!";
+
+    return 0;
+}
